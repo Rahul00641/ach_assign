2485b8a08cdf6441a61bc733e85be3a32a247b03
Rahul00641 <44252542+Rahul00641@users.noreply.github.com>
2019-11-17 12:51:29 +0530

    Add files via upload

---
+++ Assignment4.java
@@ -0,0 +1,52 @@
+package abc2;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Assignment4 {
+	
+	public static boolean palindrome(String s)
+	{
+		int i=0,j=s.length()-1;
+		while(i<j)
+		{
+			if(s.charAt(i)==s.charAt(j))
+			{
+				i++;
+				j--;
+			}
+			else
+			{
+				break;
+			}
+		}
+		if(i>=j)
+		return true;
+		else
+			return false;
+	}
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		System.out.println("Enter the String");
+		String s=in.next();
+		ArrayList<String> al=new ArrayList<String>();
+		for(int  i=0;i<s.length();i++)
+		{
+			for(int j=i+2;j<=s.length();j++)
+			{
+				al.add(s.substring(i, j));
+			}
+		}
+		for(String a : al)
+		{
+			if(palindrome(a))
+			{
+				System.out.println(a);
+				break;
+			}
+		}
+
+	}
+
+}
